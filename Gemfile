542f760bda28d61a6047a326d830ef1e8453677e
Carl Brasic <cbrasic@gmail.com>
2023-01-17 21:15:57 -0600

    Add code coverage

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gemspec
 
 gem "rake", "~> 13.0"
 gem "minitest", "~> 5.0"
+gem "simplecov", "~> 0.22"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+require "simplecov"
+
+SimpleCov.start do
+  enable_coverage :branch
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "bg_service"
-
 require "minitest/autorun"
-
 
 def assert_log_match(expected, actual)
   processed = actual.gsub(/pid \d+/, 'pid XXX')
