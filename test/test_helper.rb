28643f89d28f6d9056cff99a550ac98b6e72bee8
Carl Brasic <cbrasic@gmail.com>
2023-01-17 22:31:29 -0600

    Avoid orphan process leaks with setsid

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,10 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "bg_service"
 require "minitest/autorun"
 
+# Become a session leader to make leaking processes less likely. As leader of
+# the session, all processes we spawned will be sent SIGHUP when we exit.
+Process.setsid
+
 def assert_log_match(expected, actual)
   processed = actual.gsub(/pid \d+/, 'pid XXX')
   assert_equal(expected, processed)
